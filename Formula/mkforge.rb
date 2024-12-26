e177df96276a97df2440fc38d458cc51b1427af7
GitHub Action <action@github.com>
2024-12-26 21:28:15 +0000

    chore: update mkforge to v0.1.1

--- Formula/mkforge.rb
+++ Formula/mkforge.rb
@@ -1,25 +1,25 @@
 class Mkforge < Formula
   desc "CLI tool for generating LLM context and development tasks"
   homepage "https://mkforge.github.io"
-  version "v0.1.0"
+  version "v0.1.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://mkforge.github.io/releases/v0.1.0/mkforge-darwin-arm64"
-      sha256 "503f99f05c3e8e09cecedab196d05a59ba9df9013bd63cea3a7e15455698a577"
+      url "https://mkforge.github.io/releases/v0.1.1/mkforge-darwin-arm64"
+      sha256 "1a908350b9ddfe41d42aeb1fa3b62b3e26d97ed179d7511f01c2e12085db0bff"
     else
-      url "https://mkforge.github.io/releases/v0.1.0/mkforge-darwin-amd64"
-      sha256 "1587bd839525678847ece52b340b831ca275a6611debe1f36cddbd1791aad525"
+      url "https://mkforge.github.io/releases/v0.1.1/mkforge-darwin-amd64"
+      sha256 "11d39bda4d0721beea114160a3bfb9ee678ef13cbdeaf03cfd4bfc12c7b5abb4"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://mkforge.github.io/releases/v0.1.0/mkforge-linux-arm64"
-      sha256 "adfe9c298775288b02c450da3b86929de39195309bd953e257fc65bc8ed1be31"
+      url "https://mkforge.github.io/releases/v0.1.1/mkforge-linux-arm64"
+      sha256 "14c8b8e80379ad66e6d9c8ca9f290768fcee718ae6a158b74b91742fd059073a"
     else
-      url "https://mkforge.github.io/releases/v0.1.0/mkforge-linux-amd64"
-      sha256 "b504c15ddc3792c0e6ebc520c5046795110e6c8284b262440b55ade83a632ce5"
+      url "https://mkforge.github.io/releases/v0.1.1/mkforge-linux-amd64"
+      sha256 "6ada0d3760bae40a913e14c8006b9e9e68129e2d25c93d2dcda8a9b7e068a58c"
     end
   end
 
